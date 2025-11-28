827c1d5e93dc397e6e29b64d2bd756ec050366d8
Rhishabh01 <rhishabh89@gmail.com>
2025-11-28 22:59:29 +0530

    Updated Settings Button and Title Bars

--- app/src/main/java/com/rhishabh/myapplication/PasswordManagerApp.kt
+++ app/src/main/java/com/rhishabh/myapplication/PasswordManagerApp.kt
@@ -545,7 +545,10 @@ fun PasswordManagerApp() {
 
                                     Column(Modifier.fillMaxSize().padding(16.dp)) {
 
-
+                                        Text(
+                                            "Generated Passwords",
+                                            style = MaterialTheme.typography.headlineSmall
+                                        )
                                         Spacer(Modifier.height(8.dp))
 
                                         Button(
